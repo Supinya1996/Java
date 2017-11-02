e2036d403b5b5f62dd7826be18c934da786bf506
Jame Smith <jame_kiki@hotmail.com>
2017-11-03 06:16:05 +0700

    edit same bug

--- src/trapezoidal_rule.java
+++ src/trapezoidal_rule.java
@@ -1,42 +1,51 @@
 import java.util.Scanner;
 
 public class trapezoidal_rule {
-	public static void main(String[] args) {
+
+	public static void main(String[] args) {	// main
 		  Scanner in = new Scanner(System.in);
 		  double   area , a, b, h;
-	      int      n;        
+	      int      n;
+	      
+	      // input
 	      System.out.print("Enter a : ");
 	      a = in.nextDouble();
 	      System.out.print("Enter b : ");
 	      b = in.nextDouble();
 	      System.out.print("Enter n : ");
 	      n = in.nextInt();
-
+	      System.out.print("Enter t : ");
+	      // end input
+	      
+	      // calculator
 	      h = (b-a)/n;
 	      area = trap(a, b, n, h);
+	      // end calculator
 
-	      System.out.println("With n = " + n + " trapezoids, our estimate");
-	      System.out.print("of the area from " + a + " to " + b);
-	      System.out.println(" = " + area);		
-	}
+	      System.out.print("Area [" + a + "," + b +"] from " + n ); // output
+	      System.out.println(" = " + area);	// end output
+	} // end main
 	
-	 static double trap(double a, double b, int n, double h) {
-	       double area , x;
+	 static double trap(double a, double b, int n, double h) { // calculator Trapezoidal rule
+	       double area;
+	       double x;
 	       int i;
 	   
 	       area = (f(a) + f(b))/2.0;
 	       for (i = 1; i <= n-1; i++) {
-	           x = a + i*h;
+	           x = a + i * h;
 	           area = area + f(x);
 	       }
-	       area = area * h;
+	       area = area*h;
 	   
 	       return area;
-	   }
+	   } //end calculator Trapezoidal rule
 	 
-	 static double f(double x) {
+	 static double f(double x) { // function
 	      double return_val;  
-	      return_val = x*x + 1;
+	      return_val = 2/(x-4); // edit to change function
 	      return return_val;
-	   } 
+	   } //end function
+	 
+	 
 }
